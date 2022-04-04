0c43fbdfd5d23593d52c0b149ca0d67198992254
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-04 12:50:13 -0500

    issue one, constructor functions to pass tests for potion

---
+++ _tests_/random.test.js
@@ -0,0 +1,9 @@
+// const { TestWatcher } = require('jest');
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
